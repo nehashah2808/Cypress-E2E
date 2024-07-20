0178bec701cb01034f170152b5bfcbd6c7a72d38
“Neha <nehashah2808@gmail.com>
2024-07-20 09:48:25 +0200

    First Cypress Automation Program - Registeration Page

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  // created an env object variable to store website URL and use in Page class
+env: {
+  URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
+}
+
+});
---
+++ cypress/pages/registerPage.js
@@ -0,0 +1,73 @@
+
+
+//Naveen Automation Labs Register Page Automation 
+
+//url --- https://naveenautomationlabs.com/opencart/index.php?route=account/register
+
+
+// marking it as export , so that it can be imported in tests folder under "DESCRIBE"
+export default class registerPage{
+
+
+
+    weblocators = {
+
+        firstName : '#input-firstname',
+        lastName : '#input-lastname',
+        email : '#input-email',
+        telephone : '#input-telephone',
+        password : '#input-password',
+        passwordConfirm : '#input-confirm',
+        policyCheckbox : 'input[type="checkbox"]',
+        continue : '.btn.btn-primary'
+
+    }
+
+// Methods - Actions
+
+    // url stored in cypress config file and access via env object. 
+    // pass the Key name in env variable to access its value
+    openURL(){
+        cy.visit(Cypress.env('URL'))
+    }
+
+    enterFirstName(FName){
+
+        //access locator using cy.get
+        cy.get(this.weblocators.firstName).type(FName)
+        // we are just passing reference here. Actual data will put in fixture file
+
+    }
+
+    enterLastName(LName){
+
+        cy.get(this.weblocators.lastName).type(LName)
+    }
+
+    enterEmail(email){
+
+        cy.get(this.weblocators.email).type(email)
+    }
+
+    enterTelephone(phoneNo){
+
+        cy.get(this.weblocators.telephone).type(phoneNo)
+    }
+
+    enterPassword(password){
+
+        cy.get(this.weblocators.password).type(password)
+        cy.get(this.weblocators.passwordConfirm).type(password)
+    }
+
+    selectCheckbox(){
+        cy.get(this.weblocators.policyCheckbox).check()
+    }
+
+    clickOnContinue(){
+        cy.get(this.weblocators.continue).click()
+    }
+
+
+} //class registerPage
+
